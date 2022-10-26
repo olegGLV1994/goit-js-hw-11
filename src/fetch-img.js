8202fb7f99c2e630f3d88de95112e42ff0be598f
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-26 19:17:05 +0300

    add axios

--- src/fetch-img.js
+++ src/fetch-img.js
@@ -1,26 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '30832093-18366edc5a2dfa9c956b24a6c';
 const URL = 'https://pixabay.com/api/';
-
+let page = 1;
 function fetchImg(word) {
   return fetch(
-    `${URL}?key=${API_KEY}&q=${word}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
-  ).then(response => {
-    return response.json();
-  });
+    `${URL}?key=${API_KEY}&q=${word}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  )
+    .then(response => response.json())
+    .then(data => {
+      if (data.hits.length > 0) {
+        page += 1;
+        return data;
+      } else {
+        throw Error('photos Not Found');
+      }
+    });
 }
 
 export default { fetchImg };
-
-// export default class FetchImg {
-//   constructor() {
-//     this.page = 1;
-//     this.word = '';
-//   }
-//   fetchImg(word) {
-//     return fetch(
-//       `${URL}?key=${API_KEY}&q=${word}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
-//     ).then(response => {
-//       return response.json();
-//     });
-//   }
-// }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import FETCH from './fetch-img';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   form: document.querySelector('.search-form'),
@@ -8,32 +11,69 @@ const refs = {
 
 refs.form.addEventListener('submit', searchForm);
 refs.button.addEventListener('click', onFetchClick);
+refs.button.classList.add('is-hidden');
 
 let word = '';
 function searchForm(e) {
   e.preventDefault();
-  word = e.currentTarget.elements.searchQuery.value;
-
+  word = e.target.elements.searchQuery.value.trim();
+  if (word === '') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
   FETCH.fetchImg(word).then(onFetchResolve).catch(onFetchError);
+  refs.button.classList.remove('is-hidden');
 }
 
 function onFetchClick() {
-  FETCH.fetchImg(word).then(onFetchResolve).catch(onFetchError);
+  FETCH.fetchImg(word)
+    .then(data => {
+      const card = data.hits.map(markup).join('');
+      refs.gallery.innerHTML += card;
+      simpleLightbox(data);
+    })
+    .catch(err => {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    });
 }
 
 function onFetchResolve(photoCards) {
+  if (photoCards.totalHits === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  } else {
+    Notiflix.Notify.success(`Hooray! We found ${photoCards.totalHits} images.`);
+  }
   const card = photoCards.hits.map(markup).join('');
   refs.gallery.innerHTML = card;
+
+  simpleLightbox(photoCards);
 }
 
 function onFetchError(error) {
-  console.log(error);
+  refs.button.classList.add('is-hidden');
+  Notiflix.Notify.failure('Something went wrong');
 }
 function markup(status) {
-  const { webformatURL, tags, likes, views, comments, downloads } = status;
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = status;
+
   return `
-  <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" class='img-card' />
+  <div class="photo-card"><a href='${largeImageURL}'>
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class='img-card' /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes:</b> ${likes}
@@ -50,4 +90,12 @@ function markup(status) {
   </div>
 </div>
   `;
+}
+
+function simpleLightbox() {
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionType: 'alt',
+    captionDelay: 250,
+  }).refresh();
 }
